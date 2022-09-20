d67113ac73a8578c8355d5e9790a32aefea0f892
balvinder-arora <balvinder@grepruby.io>
2022-09-20 12:20:17 +0530

    links change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 function App() {
   return (
     <div className="App">
-     <a href="/">product</a><br/>
-     <a href="/">contact us</a><br/>
-     <a href="/">about us</a><br/>
-     <a href="/">setting</a><br/>
+     <a href="#">product</a><br/>
+     <a href="#">contact us</a><br/>
+     <a href="#">about us</a><br/>
+     <a href="#">setting</a><br/>
     </div>
   );
 }
